1d525efb8dc42385e7452a111e64bc2e99a12281
leancomp <lpcomparing@gmail.com>
2022-09-19 15:13:33 -0300

    update: badge

--- .github/update-badges.sh
+++ .github/update-badges.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/env bash
+while getopts s:r: flag
+do
+    case "${flag}" in
+        s) stage=${OPTARG};;
+        r) release=${OPTARG};;
+    esac
+done
 
+echo "Stage: $stage";
+echo "Release: $release";
+
+search_regex = "$stage-(\w*\.*\-*)*-green"
+replacement = "$stage-$release-green"
+
+sed -i "s/$search_regex/$replacement/g" README.md
